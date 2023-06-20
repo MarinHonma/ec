c15aa8153d3b365245029de0964feb5dd9e18dfa
MarinHonma <2525marin0428@gmail.com>
2023-06-21 01:30:23 +0900

    first commit

---
+++ database/migrations/2023_06_21_010523_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('top');
+            $table->string('jacket');
+            $table->string('outerwear');
+            $table->string('pants');
+            $table->string('skirt');
+            $table->string('jumpsuit');
+            $table->string('dress');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
