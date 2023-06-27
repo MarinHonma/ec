e59b7c47ded9f7324b6c431c77bcfdac33eee9f9
MarinHonma <2525marin0428@gmail.com>
2023-06-27 19:55:25 +0900

    テーブル修正2

--- database/migrations/2023_06_24_004512_create_items_table.php
+++ database/migrations/2023_06_24_004512_create_items_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->foreignId('store_id')->constrained();
             $table->foreignId('category_id')->constrained();
