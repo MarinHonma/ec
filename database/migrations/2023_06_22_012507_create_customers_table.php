65ff32f22389b5c7873b29fdf4442cea285aa2d7
MarinHonma <2525marin0428@gmail.com>
2023-07-05 16:34:08 +0900

    マルチ認証

--- database/migrations/2023_06_22_012507_create_customers_table.php
+++ database/migrations/2023_06_22_012507_create_customers_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name', 30);
-             $table->string('email', 30);
+            $table->string('email', 30);
             $table->string('password', 30);
             $table->string('shipping_address');
             $table->timestamps();
